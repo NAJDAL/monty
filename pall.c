f798b9f835f8838eb8f6c8becbfda3851ec7d340
NAJD ALEID <najdaleid6@gmail.com>
2023-11-15 00:57:26 -0800

    updated

---
+++ pall.c
@@ -0,0 +1,28 @@
+#include "monty.h"
+
+/**
+ * f_pall - Prints the elements of the stack.
+ * @head: Pointer to the stack's head.
+ * @counter: Unused parameter.
+ *
+ * This function prints the elements of the stack starting from the top.
+ * The parameter 'counter' is not utilized in this function.
+ */
+void f_pall(stack_t **head, unsigned int counter)
+{
+    stack_t *current;
+    (void)counter;
+
+    current = *head;
+
+    /* Check if the stack is empty */
+    if (current == NULL)
+        return;
+
+    /* Print each element in the stack */
+    while (current)
+    {
+        printf("%d\n", current->n);
+        current = current->next;
+    }
+}
--- push.c
+++ push.c
@@ -1,25 +1,34 @@
 #include "monty.h"
 
 /**
- * f_push - Adds a node with an integer to the stack.
- * @head: Pointer to the stack head.
- * @counter: Line number for error reporting.
+ * f_push - Adds a new node to the stack.
+ * @head: Pointer to the stack's head.
+ * @counter: Line number.
  *
- * Return: No return value.
+ * This function adds a new node to the stack with the specified integer value.
+ * The integer value is extracted from the global variable 'bus.arg'.
+ * If 'bus.arg' is not a valid integer, an error message is displayed, and
+ * the program exits with failure.
  */
 void f_push(stack_t **head, unsigned int counter)
 {
     int n, j = 0, flag = 0;
 
+    /* Check if 'bus.arg' is provided */
     if (bus.arg)
     {
+        /* Handle negative sign */
         if (bus.arg[0] == '-')
             j++;
+
+        /* Validate that 'bus.arg' is a valid integer */
         for (; bus.arg[j] != '\0'; j++)
         {
             if (bus.arg[j] > 57 || bus.arg[j] < 48)
                 flag = 1;
         }
+
+        /* Display error and exit if 'bus.arg' is not a valid integer */
         if (flag == 1)
         {
             fprintf(stderr, "L%d: usage: push integer\n", counter);
@@ -31,13 +40,18 @@ void f_push(stack_t **head, unsigned int counter)
     }
     else
     {
+        /* Display error and exit if 'bus.arg' is not provided */
         fprintf(stderr, "L%d: usage: push integer\n", counter);
         fclose(bus.file);
         free(bus.content);
         free_stack(*head);
         exit(EXIT_FAILURE);
     }
+
+    /* Convert 'bus.arg' to an integer */
     n = atoi(bus.arg);
+
+    /* Add the integer to the stack or queue based on the value of 'bus.lifi' */
     if (bus.lifi == 0)
         addnode(head, n);
     else
