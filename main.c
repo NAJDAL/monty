2808f2da42917697981ef6d5a8c9309dea313036
NAJD ALEID <najdaleid6@gmail.com>
2023-11-15 01:37:54 -0800

    updated

--- main.c
+++ main.c
@@ -1,62 +1,49 @@
 #include "monty.h"
-
-/* Global variable to manage interpreter state */
 bus_t bus = {NULL, NULL, NULL, 0};
 
 /**
- * main - Monty code interpreter
- * @argc: Number of arguments
- * @argv: Monty file location
+ * main - Entry point for the Monty bytecode interpreter.
+ * @argc: Number of command-line arguments.
+ * @argv: Array of command-line argument strings, including the Monty file location.
  *
- * Return: 0 on success
+ * Return: 0 on success.
  */
+
+
 int main(int argc, char *argv[])
 {
-    char *content;
-    FILE *file;
-    size_t size = 0;
-    ssize_t read_line = 1;
-    stack_t *stack = NULL;
-    unsigned int counter = 0;
+	char *content;
+	FILE *file;
+	size_t size = 0;
+	ssize_t read_line = 1;
+	stack_t *stack = NULL;
+	unsigned int counter = 0;
 
-    /* Validate the number of command-line arguments */
-    if (argc != 2)
-    {
-        fprintf(stderr, "USAGE: monty file\n");
-        exit(EXIT_FAILURE);
-    }
-
-    /* Open the Monty file for reading */
-    file = fopen(argv[1], "r");
-    bus.file = file;
-
-    /* Check if the file opening was successful */
-    if (!file)
-    {
-        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-        exit(EXIT_FAILURE);
-    }
-
-    /* Read and execute each line from the Monty file */
-    while (read_line > 0)
-    {
-        content = NULL;
-        read_line = getline(&content, &size, file);
-        bus.content = content;
-        counter++;
-
-        /* Execute the Monty code if a valid line is read */
-        if (read_line > 0)
-        {
-            execute(content, &stack, counter, file);
-        }
-
-        free(content);
-    }
-
-    /* Clean up and close the file */
-    free_stack(stack);
-    fclose(file);
-
-    return (0);
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	file = fopen(argv[1], "r");
+	bus.file = file;
+	if (!file)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	while (read_line > 0)
+	{
+		content = NULL;
+		read_line = getline(&content, &size, file);
+		bus.content = content;
+		counter++;
+		if (read_line > 0)
+		{
+			execute(content, &stack, counter, file);
+		}
+		free(content);
+	}
+	free_stack(stack);
+	fclose(file);
+return (0);
 }
